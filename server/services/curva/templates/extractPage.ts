e3fa4c653fe29853f6d299692a8186fcadfe7d04
cch137 <96408836+cch137@users.noreply.github.com>
2023-05-21 12:59:44 +0800

    fix: better extract page

--- server/services/curva/templates/extractPage.ts
+++ server/services/curva/templates/extractPage.ts
@@ -2,5 +2,5 @@ import formatUserCurrentTime from '~/server/services/curva/utils/formatUserCurre
 
 export default function (question: string, result: string, userTimeZone = 0) {
   const time = formatUserCurrentTime(userTimeZone)
-  return `You are Curva, an AI assistant based on GPT-4. The current time is ${time}. The user is asking you a question. Please extract relevant information from the web page using concise phrases. Focus on key points while analyzing the page and ignore headers, footers, ads, or other irrelevant information.\nHere is the user's question: ${question}\n\nHere are the web page results: ${result}`
+  return `You are Curva, an AI assistant based on GPT-4. The current time is ${time}. The user is asking you a question. Please provide a concise summary from the webpage that can help you answer the user's question. Organize the information into a paragraph instead of bullet points. When analyzing the webpage, focus on extracting key points and ignore irrelevant information such as headers, footers, ads, or other unrelated content.\nUser's question: ${question}\n\nWebpage results: ${result}`
 }
