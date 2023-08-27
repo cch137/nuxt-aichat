1e0bcffc4144fc16615e928bb798e81813b175f2
cch137 <96408836+cch137@users.noreply.github.com>
2023-08-27 15:44:57 +0800

    fix: claude prompt

--- server/services/chatbots/engines/claude-2.ts
+++ server/services/chatbots/engines/claude-2.ts
@@ -11,7 +11,7 @@ class Claude2WebChatbot {
   async ask (messages: OpenAIMessage[], options: { timezone?: number, context?: string, streamId?: string } = {}) {
     const { timezone = 0, streamId } = options
     const { question = '', context = '', isContinueGenerate } = messagesToQuestionContext(messages)
-    const prompt = context ? `QUESTION: ${question}\n\n---\n\nYou only need to respond to QUESTION. Below is the conversation you previously had with the user.\n\n---\n\n${context}` : question
+    const prompt = context ? `QUESTION: ${question}\n\n---\n\nYou only need to respond to QUESTION. After that, there is the conversation between you and the user, and no response is needed.\n\n---\n\n${context}` : question
     return {
       ...await this.core.ask(prompt, { model: 'claude-2-web', streamId }),
       // ...await this.core.ask(question, { model: 'PaLM-2' }),
