a368ddf55f78891431949883e7179d2d80fc953a
cch137 <96408836+cch137@users.noreply.github.com>
2023-10-21 17:54:36 +0800

    fix: better ls

--- pages/tools/ls.vue
+++ pages/tools/ls.vue
@@ -1,34 +1,41 @@
 <template>
   <div class="mt-8 flex-col gap-4 flex-center">
-    <h1 class="m-0">LS</h1>
-    <div class="px-4 w-full max-w-prose flex flex-col gap-4 justify-center">
-      <el-select v-model="selectedFilename" @change="fetchTree()">
-        <el-option
-          v-for="item in lsList"
-          :key="item"
-          :label="item.replaceAll('.json', '')"
-          :value="item"
-        />
-      </el-select>
-      <div class="pb-16">
-        <details v-for="chap in lsTree">
-          <summary class="chapter-title">{{ chap.chapter }}</summary>
-          <div class="flex flex-wrap gap-2 pl-4 pt-2 pb-4">
-            <el-link v-for="prob in chap.problems" :href="prob.link" target="_blank">
-              {{ prob.p }}
-            </el-link>
+    <ClientOnly>
+      <h1 class="m-0">LS</h1>
+      <div class="px-4 w-full max-w-prose flex flex-col gap-4 justify-center">
+        <el-select
+          v-model="selectedFilename"
+          @change="fetchTree()"
+          placeholder="請選擇原文書"
+          :loading="isFetchingTree"
+        >
+          <el-option
+            v-for="item in lsList"
+            :key="item"
+            :label="item.replaceAll('.json', '')"
+            :value="item"
+          />
+        </el-select>
+        <div class="pb-16">
+          <details v-for="chap in lsTree">
+            <summary class="chapter-title">{{ chap.chapter }}</summary>
+            <div class="flex flex-wrap gap-2 pl-4 pt-2 pb-4">
+              <el-link v-for="prob in chap.problems" :href="prob.link" target="_blank">
+                {{ prob.p }}
+              </el-link>
+            </div>
+          </details>
+        </div>
+        <div v-if="lsTree.length" class="flex-col flex-center pb-16">
+          <div>
+            <el-text type="info">提示：點擊下載 (Скачать) 可以取得更清晰的圖片。免責聲明：本站非以上文件之所有者，訪客若因訪問非本站頁面或資源導致任何損失，本站概不負責。</el-text>
           </div>
-        </details>
-      </div>
-      <div class="flex-col flex-center pb-16">
-        <div>
-          <el-text type="info">提示：點擊下載 (Скачать) 可以取得更清晰的圖片。免責聲明：本站非以上文件之所有者，訪客若因訪問非本站頁面或資源導致任何損失，本站概不負責。</el-text>
+        </div>
+        <div class="flex-center pb-16">
+          <el-link @click="back()">BACK</el-link>
         </div>
       </div>
-      <div class="flex-center pb-16">
-        <el-link @click="back()">BACK</el-link>
-      </div>
-    </div>
+    </ClientOnly>
   </div>
 </template>
 
@@ -54,12 +61,10 @@ function back() {
   }
 }
 
-async function fetchList() {
-  const res = await fetch('https://api.cch137.link/ls/list')
-  lsList.value = (await res.json()) as string[]
-}
+const isFetchingTree = ref(false)
 
 async function fetchTree() {
+  isFetchingTree.value = true
   const tree: {chapter: string, problems: {p:string,link:string}[]}[] = [];
   lsTree.value = [];
   const loading = useElLoading();
@@ -80,24 +85,24 @@ async function fetchTree() {
   }
   lsTree.value = tree;
   loading.close();
+  isFetchingTree.value = false
 }
 
 if (process.client) {
-  const loading = useElLoading();
-  fetchList()
-    .then(() => {
-      loading.close();
-      const q = useURLParams().get('q') || '';
-      if (!q) return;
+  (async () => {
+    const q = useURLParams().get('q') || '';
+    const req = fetch('https://api.cch137.link/ls/list');
+    lsList.value = await (await req).json() as string[]
+    if (q && !selectedFilename.value) {
       for (const book of lsList.value) {
-        console.log(book.toLowerCase(), q.toLowerCase());
         if (book.toLowerCase().includes(q.toLowerCase())) {
           selectedFilename.value = book;
-          fetchTree();
+          await fetchTree();
           break;
         }
       }
-    })
+    }
+  })();
 }
 
 const appName = useState('appName')
